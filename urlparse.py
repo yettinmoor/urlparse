5898c29ce7fac430c9ef39b24b04b133f107be16
Nicolas Lindström <nicolas.lindstroem@protonmail.com>
2019-12-19 21:14:20 +0100

    refactoring, ignoring fnf

--- urlparse.py
+++ urlparse.py
@@ -8,35 +8,29 @@ def parse(search_terms):
         sys.stderr.write('Usage: urlparse [-h] search_terms.\n')
         exit(1)
 
+    # Check if url
+    url_regex = "^(http://|https://)?(www\.)?[a-zA-Z0-9-]+\.[a-zA-Z0-9-]+.*$"
+    if re.search(url_regex, search_terms):
+        return search_terms
+
     config_dir = os.path.join(os.getenv('HOME'), '.local', 'share', 'urlparse')
 
-    # Check search engines
     try:
+        # Check search engines
         engine, *search = search_terms.split(' ')
         with open(os.path.join(config_dir, 'searchengines'), 'r') as f:
             for engine_key, engine_url in [l.rstrip().split(' ') for l in f.readlines()]:
                 if engine == engine_key:
                     return engine_url % ' '.join(search)
 
-    except FileNotFoundError:
-        sys.stderr.write('searchengines file not found.\n')
-        exit(1)
-
-    # Check bookmarks
-    try:
+        # Check bookmarks
         with open(os.path.join(config_dir, 'bookmarks'), 'r') as f:
             for bookmark_key, bookmark_url in [l.rstrip().split(' ') for l in f.readlines()]:
                 if search_terms == bookmark_key:
                     return bookmark_url
 
     except FileNotFoundError:
-        sys.stderr.write('bookmarks file not found.\n')
-        exit(1)
-
-    # Check if url
-    url_regex = "^(http://|https://)?(www\.)?[a-zA-Z0-9-]+\.[a-zA-Z0-9-]+.*$"
-    if re.search(url_regex, search_terms):
-        return search_terms
+        pass
 
     # Finally return default search
     try:
